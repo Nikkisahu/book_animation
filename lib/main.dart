a38f3b8a19b284a7c1516deb9c31b4e834b0de3b
darshan <darshanpatel1207@gmail.com>
2024-10-15 23:28:26 +0530

    Bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -30,41 +30,46 @@ class BookFlipPage extends StatefulWidget {
 }
 
 class _BookFlipPageState extends State<BookFlipPage>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
+    with TickerProviderStateMixin {
+  late List<AnimationController> _controllers;
   int currentPage = 0;
-  final int totalPages = 3;
+  final int totalPages = 6;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      duration: const Duration(milliseconds: 500),
-      vsync: this,
+    _controllers = List.generate(
+      totalPages - 1,
+      (_) => AnimationController(
+        duration: const Duration(milliseconds: 500),
+        vsync: this,
+      ),
     );
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    for (var controller in _controllers) {
+      controller.dispose();
+    }
     super.dispose();
   }
 
   void _goToNextPage() {
     if (currentPage < totalPages - 1) {
+      _controllers[currentPage].forward();
       setState(() {
         currentPage++;
       });
-      _controller.forward(from: 0.0);
     }
   }
 
   void _goToPreviousPage() {
     if (currentPage > 0) {
+      _controllers[currentPage - 1].reverse();
       setState(() {
         currentPage--;
       });
-      _controller.reverse(from: 1.0);
     }
   }
 
@@ -73,86 +78,97 @@ class _BookFlipPageState extends State<BookFlipPage>
     return Scaffold(
       backgroundColor: Colors.lightBlue[100],
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
+        child: GestureDetector(
+          onTapUp: (details) {
+            final RenderBox box = context.findRenderObject() as RenderBox;
+            final localPosition = box.globalToLocal(details.globalPosition);
+            if (localPosition.dx > box.size.width / 2) {
+              _goToNextPage();
+            } else {
+              _goToPreviousPage();
+            }
+          },
+          child: SizedBox(
+            width: 600,
+            height: 400,
+            child: Stack(
+              children: [
+                for (int i = 0; i < totalPages; i++) _buildPage(i),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPage(int pageNumber) {
+    bool isRightPage = pageNumber.isOdd;
+    return Positioned(
+      left: isRightPage ? 300 : 0,
+      child: AnimatedBuilder(
+        animation:
+            pageNumber == 0 ? _controllers[0] : _controllers[pageNumber - 1],
+        builder: (context, child) {
+          final double value = pageNumber == 0
+              ? _controllers[0].value
+              : (isRightPage
+                  ? 1 - _controllers[pageNumber - 1].value
+                  : _controllers[pageNumber - 1].value);
+          final double angle = isRightPage ? (pi * (1 - value)) : (pi * value);
+
+          return Transform(
+            transform: Matrix4.identity()
+              ..setEntry(3, 2, 0.001)
+              ..rotateY(angle),
+            alignment:
+                isRightPage ? Alignment.centerLeft : Alignment.centerRight,
+            child: Container(
               width: 300,
               height: 400,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 5,
+                    offset: Offset(2, 2),
+                  ),
+                ],
+              ),
               child: Stack(
                 children: [
-                  // Book pages
-                  for (int i = 0; i < totalPages; i++)
-                    if (i == currentPage)
-                      _buildFlippingPage(i)
-                    else
-                      _buildStaticPage(i),
+                  // Front side of the page
+                  Opacity(
+                    opacity: isRightPage ? 1 - value : value,
+                    child: Center(
+                      child: Text(
+                        'Page ${pageNumber + 1}',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ),
+                  ),
+                  // Back side of the page
+                  Transform(
+                    alignment: Alignment.center,
+                    transform: Matrix4.identity()..rotateY(pi),
+                    child: Opacity(
+                      opacity: isRightPage ? value : 1 - value,
+                      child: Center(
+                        child: Text(
+                          'Page ${pageNumber + 1}',
+                          style: TextStyle(fontSize: 24),
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.arrow_back),
-                  onPressed: currentPage > 0 ? _goToPreviousPage : null,
-                ),
-                SizedBox(width: 20),
-                IconButton(
-                  icon: Icon(Icons.arrow_forward),
-                  onPressed:
-                      currentPage < totalPages - 1 ? _goToNextPage : null,
-                ),
-              ],
-            ),
-          ],
-        ),
+          );
+        },
       ),
-    );
-  }
-
-  Widget _buildStaticPage(int pageNumber) {
-    return Container(
-      width: 300,
-      height: 400,
-      color: Colors.white,
-      child: Center(
-        child: Text(
-          'Page ${pageNumber + 1}',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildFlippingPage(int pageNumber) {
-    return AnimatedBuilder(
-      animation: _controller,
-      builder: (context, child) {
-        final double value = _controller.value;
-        final Matrix4 transform = Matrix4.identity()
-          ..setEntry(3, 2, 0.001)
-          ..rotateY(pi * value);
-
-        return Transform(
-          transform: transform,
-          alignment: Alignment.centerLeft,
-          child: Container(
-            width: 300,
-            height: 400,
-            color: Colors.white,
-            child: Center(
-              child: Text(
-                value < 0.5
-                    ? 'Page ${pageNumber + 1}'
-                    : 'Page ${pageNumber + 2}',
-                style: TextStyle(fontSize: 24),
-              ),
-            ),
-          ),
-        );
-      },
     );
   }
 }
